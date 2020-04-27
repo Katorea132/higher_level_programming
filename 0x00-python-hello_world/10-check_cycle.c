2ab48e4376b7431de4f58392a7479c2909b6ed0a
Katorea132 <katorea132@gmail.com>
2020-04-27 15:00:19 -0500

    Added fix for case of NULL list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * check_cycle - finds the loop
+ * check_cycle - finds if there's a loop on a singly linked list
  * @list: Holds the head
  * Return: 1 if loop 0 if not
  */
@@ -8,7 +8,10 @@ int check_cycle(listint_t *list)
 {
 	listint_t *past, *future;
 
-	for (past = future = list; past && future;)
+	if (list == 0)
+		return (0);
+
+	for (past = future = list; future && future->next;)
 	{
 		past = past->next;
 		future = future->next->next;
