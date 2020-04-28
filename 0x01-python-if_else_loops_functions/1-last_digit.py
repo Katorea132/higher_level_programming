0901f2b0dddb4bc2fcb98451f5178e2f5fb17e7c
Katorea132 <katorea132@gmail.com>
2020-04-28 13:09:53 -0500

    Added propper handler for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 print("The last digit of", number, end = '')
 if number < 0:
-    number *= -1
-print(" is", number % 10, "and is", end = '')
-if number % 10 > 5:
-    print(" greater than 5")
-elif number % 10 == 0:
-    print(" and is", number % 10)
+    print(" is", number % -10, "and is", end = '')
+    if number % -10 == 0:
+        print(" 0")
+    else:
+        print(" less than 6 and not 0")
 else:
-    print(" less than 6 and not 0")
+    print(" is", number % 10, "and is", end = '')
+    if number % 10 > 5:
+        print(" greater than 5")
+    elif number % 10 == 0:
+        print(" and is 0")
+    else:
+        print(" less than 6 and not 0")
