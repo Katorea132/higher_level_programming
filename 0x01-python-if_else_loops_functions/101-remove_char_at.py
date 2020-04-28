f1d8c029607f7b2c5fa2965fe240705aa5718307
Katorea132 <katorea132@gmail.com>
2020-04-28 17:53:49 -0500

    Added 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    '''
+    First attempt at a docstring: This function removes a character of a string
+    in a given index'''
+    for c in str:
+        if (n >= 0):
+            return str[:n] + [n + 1:]
+        else:
+            return str
