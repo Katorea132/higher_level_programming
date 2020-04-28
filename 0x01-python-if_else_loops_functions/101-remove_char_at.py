57812ab5215ae17f3560db658130d21f0e1b9d57
Katorea132 <katorea132@gmail.com>
2020-04-28 17:57:46 -0500

    Removed innecesary for

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,8 +3,7 @@ def remove_char_at(str, n):
     '''
     First attempt at a docstring: This function removes a character of a string
     in a given index'''
-    for c in str:
-        if (n >= 0):
-            return str[:n] + str[n + 1:]
-        else:
-            return str
+    if (n >= 0):
+        return str[:n] + str[n + 1:]
+    else:
+        return str
