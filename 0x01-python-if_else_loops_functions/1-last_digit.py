2a4bd45ef46ed8ebae2e622da9a0992b228dea3d
Katorea132 <katorea132@gmail.com>
2020-04-28 13:20:22 -0500

    Added fix for 0 case in possitive numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,6 +13,6 @@ else:
     if number % 10 > 5:
         print(" greater than 5")
     elif number % 10 == 0:
-        print(" and is 0")
+        print(" 0")
     else:
         print(" less than 6 and not 0")
