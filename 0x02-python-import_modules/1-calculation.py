0b8985856374385f30ae1eabc3b3b1a720f43ddf
Katorea132 <katorea132@gmail.com>
2020-04-30 14:02:42 -0500

    Fixed the multipel lines print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,8 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print('''{0} + {1} = {2}
+    print("""{0} + {1} = {2}
     {0} - {1} = {3}
     {0} * {1} = {4}
-    {0} / {1} = {5}'''.format(a, b, add(a, b), sub(a, b), mul(a, b), div(a, b)))
+    {0} / {1} = {5}\
+    """.format(a, b, add(a, b), sub(a, b), mul(a, b), div(a, b)))
