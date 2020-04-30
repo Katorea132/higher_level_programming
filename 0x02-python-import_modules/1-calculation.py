2e829983cfe8a79b6d0fc087064bfd30a46a702a
Katorea132 <katorea132@gmail.com>
2020-04-30 14:18:37 -0500

    Added fix for extra spaces

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     a = 10
     b = 5
     print("""{0} + {1} = {2}
-    {0} - {1} = {3}
-    {0} * {1} = {4}
-    {0} / {1} = {5}
-    """.format(a, b, add(a, b), sub(a, b), mul(a, b), div(a, b)), end='')
+{0} - {1} = {3}
+{0} * {1} = {4}
+{0} / {1} = {5}
+""".format(a, b, add(a, b), sub(a, b), mul(a, b), div(a, b)), end='')
