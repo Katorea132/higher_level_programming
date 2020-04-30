b21782c584e366811913506961af01813b9231eb
Katorea132 <katorea132@gmail.com>
2020-04-30 16:49:58 -0500

    Fixed mistakes in spelling

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
         elif op == '-':
             print(a, op, b, '=', sub(a, b))
         else:
-            print('Unknown operator. Avaliable operators: +, -, * and /')
+            print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
