df56cc38873e1b180d7887c9a4968a9970b5f240
Katorea132 <katorea132@gmail.com>
2020-04-30 17:03:43 -0500

    Imported the calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv[1:]) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
