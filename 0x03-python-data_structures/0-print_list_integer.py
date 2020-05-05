c90e0f3ec383977b47b1dcfb2193b2d630e08ba2
Katorea132 <katorea132@gmail.com>
2020-05-05 14:19:30 -0500

    fixed an extra semicolon

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     '''This is a function that prints all the elements of a list'''
     for x in my_list:
-        print('{}'.format(x));
+        print('{}'.format(x))
