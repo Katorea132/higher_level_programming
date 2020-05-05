60bb16879ec8563ff7ec4e2a6d7a1dfb8a5c13bf
Katorea132 <katorea132@gmail.com>
2020-05-05 14:24:02 -0500

    Attempt at fixing this

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    '''This is a function that prints all the elements of a list'''
+    '''
+    This is a function that prints all the elements of a list
+    '''
     for x in range(len(my_list)):
         print('{}'.format(my_list[x]))
