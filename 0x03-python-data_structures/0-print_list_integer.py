bb802c0b8ce24857e771dc196fbb9b15ef5007e7
Katorea132 <katorea132@gmail.com>
2020-05-05 14:22:23 -0500

    Fixed printing logic

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     '''This is a function that prints all the elements of a list'''
-    for x in my_list:
-        print('{}'.format(x))
+    for x in range(len(my_list)):
+        print('{}'.format(my_list[x]))
