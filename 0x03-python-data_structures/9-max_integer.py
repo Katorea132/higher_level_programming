90d5227c77308208ca1c2201d2f05de512de9727
Katorea132 <katorea132@gmail.com>
2020-05-05 17:28:06 -0500

    Changed logic just in case the lists contains only negatives

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max = 0
+        max = my_list[0]
         for n in my_list:
             if max < n:
                 max = n
