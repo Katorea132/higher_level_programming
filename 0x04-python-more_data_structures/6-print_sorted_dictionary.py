033ad25aabf7e3b06c35cc3680e65b966ab710dd
Katorea132 <katorea132@gmail.com>
2020-05-07 17:09:43 -0500

    FIxed logic for when dictionary is empty

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(dic):
-    print('\n'.join(['{}: {}'.format(l, v) for l, v in sorted(dic.items())]))
+def print_sorted_dictionary(d):
+    if d:
+        print('\n'.join(['{}: {}'.format(l, v) for l, v in sorted(d.items())]))
