8ef9739c20fb1650fb045b377e38cf6f563d554a
Katorea132 <katorea132@gmail.com>
2020-05-07 20:31:47 -0500

    Fixed it to work outside the function

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary.items():
+    for k, v in dict(a_dictionary).items():
         if value in v:
             del a_dictionary[k]
-    return dict({k: v for (k, v) in a_dictionary.items() if value not in v})
+    return a_dictionary
