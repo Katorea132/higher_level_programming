da50844a076aa892e92cafc2711de693e0e88054
Katorea132 <katorea132@gmail.com>
2020-05-18 15:03:57 -0500

    Fixed ven more errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for numbers in my_list:
         try:
             if nums <= x:
-                print('{d}'.format(numbers), end='')
+                print('{}'.format(numbers), end='')
                 nums += 1
             else:
                 break
