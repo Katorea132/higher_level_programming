4bb17908ae516858952644473de903ee9fecd1b3
Katorea132 <katorea132@gmail.com>
2020-05-18 14:59:34 -0500

    Added

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nums = 0
+    for numbers in my_list:
+        try:
+            if nums <= x:
+                print('{d}'.format(numbers), end='')
+                nums += 1
+            else:
+                break;
+            except TypeError:
+                continue;
+    print()
+    return nums
