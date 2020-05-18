1c0ca9380634f8a4292fcbca97e02edcc15dae11
Katorea132 <katorea132@gmail.com>
2020-05-18 17:14:55 -0500

    Fixed missing upper case

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,10 +4,10 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('too far')
+                raise Exception('Too far')
             else:
                 result += (a ** b) / i
         except:
             result = b + a
             break
-    result result
+    return result
