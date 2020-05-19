958e37d4b4cbc5a912965a6c3931288ca339f906
Katorea132 <katorea132@gmail.com>
2020-05-19 18:33:37 -0500

    Fixed pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A file to declare a square and it's size"""
 
+
 class Square:
     """
     A square class with it's size
@@ -9,5 +10,13 @@ class Square:
         _size (int): Holds the size of the square, private.
     """
     def __init__(self, size):
-        """This initializes the private value size"""
+        """
+        Initializes the square's size
+
+        Args:
+            size (int): Holds the size
+
+        Returns:
+            None
+        """
         self.__size = size
