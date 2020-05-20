7e59fb02c4982594c549d91cc2b8eac3c310c646
Katorea132 <katorea132@gmail.com>
2020-05-19 19:35:33 -0500

    Added 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+This will initialize a square, checking for an adecuate size value
+and type
+"""
+
+
+class Square:
+    """
+    Class to define a square with propper size
+    """
+    def __init__(self, size=0):
+        """Initializes the size
+
+        Args:
+            size (int): Holds the size, must be a number not
+            below 0. Defaults to 0.
+        Raises:
+            TypeError: In case value is not an int
+            ValueError: In case value is below 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area according to the size
+
+        Returns:
+            int: area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for size
+
+        Returns:
+            int: Size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, siz):
+        """setter for size
+
+        Args:
+            siz (int): New size of square, must be 0 or above, also
+            a number
+
+        Raises:
+            TypeError: When value is not an integer
+            ValueError: When value is below 0
+        """
+        if type(siz) is not int:
+            raise TypeError("size must be an integer")
+        elif siz < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = siz
+
+    def my_print(self):
+        """Prints a square or an empty line if size 0
+        """
+        r = range(self.__size)
+        if self.__size == 0:
+            print()
+        else:
+            print("\n".join(["".join(["#" for i in r]) for y in r]))
