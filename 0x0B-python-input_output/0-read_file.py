06c8edfcef488659c9197178453a5b5ea87783ab
Katorea132 <katorea132@gmail.com>
2020-06-03 18:15:25 -0500

    Added 0 and README file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module constains a function to read and print the
+contents of a file with utf 8 encoding
+"""
+
+
+def read_file(filename=""):
+    """Prints the file
+
+    Args:
+        filename (str): The name of the object file. Defaults to "".
+    """
+    with open(filename) as f:
+        print(f.read(), end="")
