3035516978ddacc634aa62f0e437628f3d63c6af
Katorea132 <katorea132@gmail.com>
2020-06-08 18:39:48 -0500

    Added more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,7 +14,7 @@ class TestBase(unittest.TestCase):
     def setUp(self):
         """Resets thingies for the test
         """
-        Base._base__nb_objects = 0
+        Base._Base__nb_objects = 0
 
     def test_propperclass(self):
         """Tests for the propper class
@@ -27,30 +27,53 @@ class TestBase(unittest.TestCase):
     def test_propperId(self):
         """Tests for the propper ID
         """
-        self.assertEqual(Base._base__nb_objects, 0)
+        self.assertEqual(Base._Base__nb_objects, 0)
         b = Base()
-        self.assertEqual(Base._base__nb_objects, 1)
+        self.assertEqual(Base._Base__nb_objects, 1)
         self.assertEqual(b.id, 1)
         b = Base(132)
-        self.assertEqual(Base._base__nb_objects, 1)
+        self.assertEqual(Base._Base__nb_objects, 1)
         self.assertEqual(b.id, 132)
         b = Base()
-        self.assertEqual(Base._base__nb_objects, 2)
+        self.assertEqual(Base._Base__nb_objects, 2)
         self.assertEqual(b.id, 2)
         b = Base()
-        self.assertEqual(Base._base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
         self.assertEqual(b.id, 3)
-        self.assertEqual(Base._base__nb_objects, b.id)
+        self.assertEqual(Base._Base__nb_objects, b.id)
 
     def test_Init(self):
         """Tests for propper initialization
         """
         with self.assertRaises(TypeError) as err:
             Base(1, 2)
-        e = "__init__() takes from 1 to 2 positional arguments but 3\
-            were given"
+        e = "__init__() takes from 1 to 2 positional arguments but 3 \
+were given"
         self.assertEqual(str(err.exception), e)
         b = Base("pizza")
         self.assertEqual("pizza", b.id)
         b = Base(id="pizza")
         self.assertEqual("pizza", b.id)
+
+    def test_toJson(self):
+        """Tests transformation to a JSON
+        """
+        test = [{"pizza": 1, "pineapple": 1}]
+        self.assertEqual(Base.to_json_string(test),
+                         '[{"pizza": 1, "pineapple": 1}]')
+        test = [{"pizza": 1, "pineapple": 1}, {"1": 2}]
+        self.assertEqual(Base.to_json_string(test),
+                         '[{"pizza": 1, "pineapple": 1}, {"1": 2}]')
+        test = [{}]
+        self.assertEqual(Base.to_json_string(test),
+                         '[{}]')
+        test = [{}, {}]
+        self.assertEqual(Base.to_json_string(test),
+                         '[{}, {}]')
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        with self.assertRaises(TypeError) as err:
+            Base.to_json_string()
+        expect = "to_json_string() missing 1 required positional \
+argument: 'list_dictionaries'"
+        self.assertEqual(str(err.exception), expect)
