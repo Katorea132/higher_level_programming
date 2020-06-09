941a1df7cb5e6fba76e77626e07374b77f2025cb
Katorea132 <katorea132@gmail.com>
2020-06-09 01:02:08 -0500

    This is madness

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""THis is for tests for rectangles
+"""
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import io
+import contextlib
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    """Class to test
+    """
+    def setUp(self):
+        """Sets up stuff
+        """
+        Base._Base__nb_objects = 0
+
+    def test_Subclassness(self):
+        """Tests if inherited from base
+        """
+        self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_Init(self):
+        """Tests the initializer
+        """
+        r = Rectangle(1, 2)
+        self.assertEqual(r.id, 1)
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle()
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle(1)
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle(1, 3, 2, 4, 5, 6)
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle("pizza", 1)
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle(1, "pizza")
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle(1, 2, "Pizza")
+        with self.assertRaises(TypeError) as err:
+            r = Rectangle(1, 2, 3, "Pizza")
+        with self.assertRaises(ValueError) as err:
+            r = Rectangle(-1, 2, 3)
+        with self.assertRaises(ValueError) as err:
+            r = Rectangle(0, 2, 3)
+        with self.assertRaises(ValueError) as err:
+            r = Rectangle(1, -2, 3)
+        with self.assertRaises(ValueError) as err:
+            r = Rectangle(1, 0, 3)
+        with self.assertRaises(ValueError) as err:
+            r = Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError) as err:
+            r = Rectangle(1, 2, 3, -4)
+        r = Rectangle(width=5, x=6, id=999, y=1, height=100)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.x, 6)
+        self.assertEqual(r.y, 1)
+        self.assertEqual(r.id, 999)
+        self.assertEqual(r.height, 100)
+        r.width = 10
+        self.assertEqual(r.width, 10)
+        r.id = 123123123
+        self.assertEqual(r.id, 123123123)
+        r.x = 0
+        r.y = 0
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+
+    def test_Area(self):
+        with self.assertRaises(TypeError) as err:
+            Rectangle.area()
+        r = Rectangle(1, 2)
+        self.assertEqual(r.area(), 2)
+
+    def test_Display(self):
+        with self.assertRaises(TypeError) as err:
+            Rectangle.display()
+        r = Rectangle(1, 1)
+        out = io.StringIO()
+        with contextlib.redirect_stdout(out):
+            r.display()
+        self.assertEqual(out.getvalue(), "#\n")
+        r = Rectangle(1, 1, 1, 1)
+        out = io.StringIO()
+        with contextlib.redirect_stdout(out):
+            r.display()
+        self.assertEqual(out.getvalue(), "\n #\n")
+
+    def test_toStr(self):
+        with self.assertRaises(TypeError) as err:
+            Rectangle.__str__()
+        r = Rectangle(1, 1)
+        self.assertEqual(str(r), "[Rectangle] (1) 0/0 - 1/1")
+        r = Rectangle(1, 1, 1, 1, 1)
+        self.assertEqual(str(r), "[Rectangle] (1) 1/1 - 1/1")
