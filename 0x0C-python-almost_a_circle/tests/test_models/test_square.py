90d7168174a9dbb93953bc6bd6eaacfafbfba3b2
Katorea132 <katorea132@gmail.com>
2020-06-09 13:59:35 -0500

    Added everything :D

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -20,45 +20,35 @@ class TestRectangle(unittest.TestCase):
     def test_Subclassness(self):
         """Tests if inherited from base
         """
-        self.assertTrue(issubclass(Rectangle, Base))
+        self.assertTrue(issubclass(Square, Base))
 
     def test_Init(self):
         """Tests the initializer
         """
-        r = Rectangle(1, 2)
+        r = Square(1, 2)
         self.assertEqual(r.id, 1)
         with self.assertRaises(TypeError) as err:
-            r = Rectangle()
+            r = Square()
         with self.assertRaises(TypeError) as err:
-            r = Rectangle(1)
+            r = Square("pizza", 1)
         with self.assertRaises(TypeError) as err:
-            r = Rectangle(1, 3, 2, 4, 5, 6)
+            r = Square(1, "pizza")
         with self.assertRaises(TypeError) as err:
-            r = Rectangle("pizza", 1)
-        with self.assertRaises(TypeError) as err:
-            r = Rectangle(1, "pizza")
-        with self.assertRaises(TypeError) as err:
-            r = Rectangle(1, 2, "Pizza")
-        with self.assertRaises(TypeError) as err:
-            r = Rectangle(1, 2, 3, "Pizza")
+            r = Square(1, 2, "Pizza")
         with self.assertRaises(ValueError) as err:
-            r = Rectangle(-1, 2, 3)
+            r = Square(-1, 2, 3)
         with self.assertRaises(ValueError) as err:
-            r = Rectangle(0, 2, 3)
+            r = Square(0, 2, 3)
         with self.assertRaises(ValueError) as err:
-            r = Rectangle(1, -2, 3)
+            r = Square(1, -2, 3)
         with self.assertRaises(ValueError) as err:
-            r = Rectangle(1, 0, 3)
-        with self.assertRaises(ValueError) as err:
-            r = Rectangle(1, 2, -3)
-        with self.assertRaises(ValueError) as err:
-            r = Rectangle(1, 2, 3, -4)
-        r = Rectangle(width=5, x=6, id=999, y=1, height=100)
-        self.assertEqual(r.width, 5)
-        self.assertEqual(r.x, 6)
+            r = Square(1, 2, -3)
+        r = Square(size=1, x=1, y=1, id=1)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
-        self.assertEqual(r.id, 999)
-        self.assertEqual(r.height, 100)
+        self.assertEqual(r.id, 1)
+        self.assertEqual(r.height, 1)
         r.width = 10
         self.assertEqual(r.width, 10)
         r.id = 123123123
@@ -67,26 +57,29 @@ class TestRectangle(unittest.TestCase):
         r.y = 0
         self.assertEqual(r.x, 0)
         self.assertEqual(r.y, 0)
+        r.size = 5
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 5)
 
     def test_Area(self):
         """this is for the area
         """
         with self.assertRaises(TypeError) as err:
-            Rectangle.area()
-        r = Rectangle(1, 2)
-        self.assertEqual(r.area(), 2)
+            Square.area()
+        r = Square(2, 2)
+        self.assertEqual(r.area(), 4)
 
     def test_Display(self):
         """This is for displaying
         """
         with self.assertRaises(TypeError) as err:
-            Rectangle.display()
-        r = Rectangle(1, 1)
+            Square.display()
+        r = Square(1, 1)
         out = io.StringIO()
         with contextlib.redirect_stdout(out):
             r.display()
-        self.assertEqual(out.getvalue(), "#\n")
-        r = Rectangle(1, 1, 1, 1)
+        self.assertEqual(out.getvalue(), " #\n")
+        r = Square(1, 1, 1, 1)
         out = io.StringIO()
         with contextlib.redirect_stdout(out):
             r.display()
@@ -96,18 +89,18 @@ class TestRectangle(unittest.TestCase):
         """This is to check str
         """
         with self.assertRaises(TypeError) as err:
-            Rectangle.__str__()
-        r = Rectangle(1, 1)
-        self.assertEqual(str(r), "[Rectangle] (1) 0/0 - 1/1")
-        r = Rectangle(1, 1, 1, 1, 1)
-        self.assertEqual(str(r), "[Rectangle] (1) 1/1 - 1/1")
+            Square.__str__()
+        r = Square(1, 1)
+        self.assertEqual(str(r), "[Square] (1) 1/0 - 1")
+        r = Square(1, 1, 1, 1)
+        self.assertEqual(str(r), "[Square] (1) 1/1 - 1")
 
     def test_Update(self):
         """Tests updates
         """
         with self.assertRaises(TypeError) as err:
-            Rectangle.update()
-        r = Rectangle(1, 1)
+            Square.update()
+        r = Square(1, 1)
         r.update(132)
         self.assertEqual(r.id, 132)
         r.update(132, 132, 132, 132, 132)
@@ -121,16 +114,12 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.x, 132)
         self.assertEqual(r.y, 132)
         with self.assertRaises(ValueError):
-            r.update(132, -132)
-        with self.assertRaises(ValueError):
             r.update(132, 132, -132)
         with self.assertRaises(ValueError):
             r.update(132, 132, 132, -132)
-        with self.assertRaises(ValueError):
-            r.update(132, 132, 132, 132, -132)
         r.update(id=133)
         self.assertEqual(r.id, 133)
-        r.update(id=133, height=133, width=133, x=133, y=133)
+        r.update(id=133, height=133, size=133, x=133, y=133)
         self.assertEqual(r.width, 133)
         self.assertEqual(r.height, 133)
         self.assertEqual(r.x, 133)
@@ -140,10 +129,10 @@ class TestRectangle(unittest.TestCase):
         """Tests to dict function
         """
         with self.assertRaises(TypeError) as err:
-            Rectangle.to_dictionary()
-        r = Rectangle(1, 1)
+            Square.to_dictionary()
+        r = Square(1, 1)
         self.assertEqual(r.to_dictionary(),
-                         {'width': 1, 'id': 1, 'height': 1, 'x': 0, 'y': 0})
-        r = Rectangle(1, 1, 1, 1, 1)
+                         {'id': 1, 'size': 1, 'x': 1, 'y': 0})
+        r = Square(1, 1, 1, 1)
         self.assertEqual(r.to_dictionary(),
-                         {'width': 1, 'id': 1, 'height': 1, 'x': 1, 'y': 1})
+                         {'size': 1, 'id': 1, 'x': 1, 'y': 1})
