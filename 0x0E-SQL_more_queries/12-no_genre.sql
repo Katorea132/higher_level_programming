a8c399a082c7d8596a0a20fab6d2f056b05d0d40
Katorea132 <katorea132@gmail.com>
2020-07-14 18:17:22 -0500

    Added 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+-- ayayaya
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
