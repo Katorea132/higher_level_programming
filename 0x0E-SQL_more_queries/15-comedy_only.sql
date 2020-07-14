7674b29096cc48ae0ab8fd5b81a2a9685113bced
Katorea132 <katorea132@gmail.com>
2020-07-14 18:36:35 -0500

    Fixed and

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,2 @@
 -- man
-SELECT tv_shows FROM tv_genres, tv_shows_genres, tv_shows WHERE tv_genres.id = tv_shows_genres.genre_id AND tv_shows_genres.show_id = tv_shows.id and tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
+SELECT tv_shows FROM tv_genres, tv_shows_genres, tv_shows WHERE tv_genres.id = tv_shows_genres.genre_id AND tv_shows_genres.show_id = tv_shows.id AND tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
