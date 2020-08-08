102b5944f4a62bda7cab8f71a94867242f27cce1
Katorea132 <katorea132@gmail.com>
2020-08-08 18:45:35 -0500

    Missed tablename

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """Module that contains states class
 """
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     """CLass
@@ -12,6 +16,7 @@ class State(Base):
     Args:
         Base (class): Super Class
     """
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
