8c8fecb1eceea6fd358ae6d2c1a0c3021151aec6
Katorea132 <katorea132@gmail.com>
2020-08-08 22:16:54 -0500

    Added 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Explanation
+"""
+
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sys import argv as par
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    args = 'mysql+mysqldb://{}:{}'.format(par[1], par[2])
+    args += '@localhost:3306/{}'.format(par[3])
+    eng = create_engine(args, pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+
+    ses = sessionmaker(bind=eng)
+    Sess = ses()
+    state = State(name="Louisiana")
+    Sess.add(state)
+    state = Sess.query(State).get(2)
+    state.name = "New Mexico"
+    Sess.commit()
+    Sess.close()
+    
