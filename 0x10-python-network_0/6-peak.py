9b287ff1a5a223ca0e6efe1e2240fe59cbddd571
Katorea132 <katorea132@gmail.com>
2020-08-18 18:45:13 -0500

    Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,5 @@
 
 def find_peak(lili):
     if lili:
-        if len(lili) > 1:
-            if lili[0] > lili[1]:
-                return lili[0]
-            elif lili[-1] > lili[-2]:
-                return lili[-1]
         return sorted(lili)[-1]
     return None
