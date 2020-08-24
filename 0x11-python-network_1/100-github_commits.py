e3af160cfda432949cf92d622661d66fd50efac4
Katorea132 <katorea132@gmail.com>
2020-08-24 17:48:03 -0500

    fixed limiter

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 {}/{}/commits'.format(argv[1], argv[2]))
     counter = 0
     for j in r.json():
-        if counter > 8:
+        if counter > 9:
             break
         print('{}: {}'.format(j['sha'], j['commit']['author']['name']))
         counter += 1
