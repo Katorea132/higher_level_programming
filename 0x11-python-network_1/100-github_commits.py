84da49cd35f34cd7d03c0e70dd2c3c6ec9c38bb6
Katorea132 <katorea132@gmail.com>
2020-08-24 18:02:25 -0500

    Added back the counter

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/\
 {}/{}/commits'.format(argv[2], argv[1]))
     counter = 0
-    for j in r.json():
+    for j in r.json()[:10]:
         print('{}: {}'
               .format(j.get('sha'), j.get('commit').get('author').get('name')))
