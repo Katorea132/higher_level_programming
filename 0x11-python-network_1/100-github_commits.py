7e3980a4b07f3d28915ebba231e321095144f56d
Katorea132 <katorea132@gmail.com>
2020-08-24 18:01:26 -0500

    Fixed order of arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,8 +5,8 @@ from sys import argv
 
 if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/\
-{}/{}/commits'.format(argv[1], argv[2]))
+{}/{}/commits'.format(argv[2], argv[1]))
     counter = 0
-    for j in r.json()[:10]:
+    for j in r.json():
         print('{}: {}'
               .format(j.get('sha'), j.get('commit').get('author').get('name')))
