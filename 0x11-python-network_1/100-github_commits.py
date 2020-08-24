9c2c6cfc11d5b12fdaf4b9e5e0f4ff22ac68fe34
Katorea132 <katorea132@gmail.com>
2020-08-24 17:45:32 -0500

    Added a limiter to 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,5 +6,9 @@ from sys import argv
 if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/\
 {}/{}/commits'.format(argv[1], argv[2]))
+    counter = 0
     for j in r.json():
+        if counter > 8:
+            break
         print('{}: {}'.format(j['sha'], j['commit']['author']['name']))
+        counter += 1
