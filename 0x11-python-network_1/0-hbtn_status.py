7f6f1187413e6dadd4d65858011b367ba48800bb
Katorea132 <katorea132@gmail.com>
2020-08-24 17:32:36 -0500

    Fixed aton of typos

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@
 from urllib import request
 
 if __name__ == '__main__':
-    with request.urlopen('htts://intranet.hbtn.io/status') as r:
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
         r = r.read()
         print('Body response:')
         print('\t- type: {}\n\t- content: {}\n\t- \
-    utf8 content: {}'.format(type(r), r, r.decode('utf-8')))
+utf8 content: {}'.format(type(r), r, r.decode('utf-8')))
