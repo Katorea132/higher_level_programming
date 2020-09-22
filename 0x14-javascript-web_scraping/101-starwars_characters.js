5d8d575325ec69577e202ccab1b4cf60461b7c9f
Katorea132 <katorea132@gmail.com>
2020-09-22 14:21:21 -0500

    Adding 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const req = require('request');
+req(`http://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, bod) => {
+  if (err) throw new Error(err);
+  for (const character of JSON.parse(bod).characters) {
+    req(character, (err, res, bod) => {
+      if (err) throw new Error(err);
+      console.log(JSON.parse(bod).name);
+    });
+  }
+});
